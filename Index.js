afa1208f2b22c2e427f58d5059b06b924da09eb1
LuizRullesz <luiz.lzr44@gmail.com>
2025-07-19 17:09:32 -0300

    atualizando index.js

--- Index.js
+++ Index.js
@@ -1,46 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const cpfRoutes = require('./routes/cpfRoutes');
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
-function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]+/g, '');
-    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+app.use(express.static(path.join(__dirname, 'public')));
 
-    let soma = 0;
-    for (let i = 0; i < 9; i++) {
-        soma += parseInt(cpf.charAt(i)) * (10 - i);
-    }
-    let resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(cpf.charAt(9))) return false;
 
-    soma = 0;
-    for (let i = 0; i < 10; i++) {
-        soma += parseInt(cpf.charAt(i)) * (11 - i);
-    }
-    resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(cpf.charAt(10))) return false;
-
-    return true;
-}
-
-app.post('/validar-cpf', (req, res) => {
-    const { cpf } = req.body;
-  
-    if (!cpf) {
-      return res.status(400).json({ erro: 'CPF não enviado' });
-    }
-  
-    const valido = validarCPF(cpf);
-    return res.json({ cpf, valido });
-  });
+app.use('/api', cpfRoutes);
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+  console.log('Servidor rodando na porta 3000');
 });
-
